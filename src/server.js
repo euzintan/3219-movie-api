9f0a8e758f8efb8304703061d156ef97d11d28bc
Tan Eu Zin <taneuzin@u.nus.edu>
2022-09-14 00:04:41 +0800

    fixes deployment error due to port

--- src/server.js
+++ src/server.js
@@ -35,9 +35,9 @@ let movies = {
     description: "Catch em all",
   },
 };
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} 3000`);
 });
 
 app.all("/", (req, res, next) => {
